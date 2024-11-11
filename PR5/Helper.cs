1248acca262cfec7fbc7eb3d90d8910d28114e1b
attys192 <kontrasitovich2306@gmail.com>
2024-11-11 21:13:05 +0700

    Добавьте файлы проекта.

---
+++ PR5/Helper.cs
@@ -0,0 +1,56 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR5.Models;
+namespace PR5
+{
+    internal class Helper
+    {
+        private static AutobaseEntities1 _context;
+        public static AutobaseEntities1 GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new AutobaseEntities1();
+            }
+            return _context;
+        }
+        public void CreateUser(Users user)
+        {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            
+           
+        }
+        private void UpdateUser(Users users)
+        {
+            _context.Entry(users).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void RemoveUser(int UserID)
+        {
+            var users = _context.Users.Find(UserID);
+            _context.Users.Remove(users);
+            _context.SaveChanges();
+        }
+
+        public List<Users> FiltrUsers()
+        {
+            return _context.Users.OrderBy(x => x.Login.StartsWith("M") || x.Login.StartsWith("A")).ToList();
+        }
+
+        public List<Users> SortUsers()
+        {
+            return _context.Users.OrderBy(x => x.Login).ToList();
+        }
+
+        public string GetUserTypes(Employees employee)
+        {
+
+            return employee.Positions.PositionID.ToString();
+        }
+    }
+}
---
+++ PR5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using PR5.Models;
+using HashPassword;
+namespace PR5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper.GetContext();
+            Helper helper = new Helper();
+            HashPassword.Hash hash = new HashPassword.Hash();
+
+            Console.WriteLine("Введите логин: ");
+            string login = Console.ReadLine();
+            Console.WriteLine("Введите пароль: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Введите ID сотрудника: ");
+            int EmployeeID = int.Parse(Console.ReadLine());
+
+            string hashPassw = hash.hashPassword(password);
+            Console.WriteLine($"Хэшированный пароль: {hashPassw}");
+
+            Users users = new Users();
+            users.Login = login;
+            users.Password = hashPassw;
+            users.EmployeeID = EmployeeID;
+
+            helper.CreateUser(users);
+
+            Console.WriteLine("Учетная запись добавлена");
+
+            Console.ReadLine();
+        }
+    }
+}
